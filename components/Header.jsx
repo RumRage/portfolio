1559075715f1c5b2662440f13f09f0b01cafbf59
rumrage <claravillarruel4@gmail.com>
2023-12-29 19:55:47 -0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+//componentes
+import Logo from "./Logo";
+import ThemeToggler from "./ThemeToggler";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+
+    //quitar evento
+    return () => window.removeEventListener("scroll", scrollYPos);
+  });
+
+  return (
+    <header
+      className={`${
+        header
+          ? "py-4 bg-white shadow-lg dark:bg-accent"
+          : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all ${pathname === "/" && "bg-[#fef9f5]"}`}
+    >
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* Nav */}
+            <Nav
+              containerStyles="hidden xl:flex items-center gap-x-8"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+            {/* Mobile nav */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//Iconos
+import { Download, Send } from "lucide-react";
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+
+const Hero = () => {
+  return <section>Hero</section>;
+};
+
+export default Hero;
