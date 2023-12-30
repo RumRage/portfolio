38696ac92b3dde1e0fb166bf9c68dbe6a47ead4a
rumrage <claravillarruel4@gmail.com>
2023-12-30 16:21:08 -0300

    swiper listo

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 //Componentes
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Projects from "./projects/page";
+import Projects from "@/components/Projects";
+import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Cta />
     </main>
   );
 }
---
+++ components/Cta.jsx
@@ -0,0 +1,16 @@
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const Cta = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <h2></h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cta;
