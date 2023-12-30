c0b56238b033c842f8053578cbbdf376585dd89a
rumrage <claravillarruel4@gmail.com>
2023-12-30 16:54:15 -0300

    subiendo

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+//Componentes
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/Pro";
 
 const Projects = () => {
   return <div>Projects</div>;
