11166252b48a99bda934fbb1aeca1ce1b71935b9
rumrage <claravillarruel4@gmail.com>
2023-12-29 22:27:51 -0300

    badge

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,14 +13,18 @@ import {
 import { Button } from "./ui/button";
 import Socials from "./Socials";
 import MyImg from "./MyImg";
+import Badge from "./Badge";
 
 const Hero = () => {
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none ">
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom dark:bg-none bg-cover">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* Texto */}
-          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 ">
+          <div
+            className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0
+          text-center xl:text-left"
+          >
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
               Web Developer
             </div>
@@ -55,21 +59,39 @@ const Hero = () => {
           </div>
           {/* Imagen */}
           <div className="hidden xl:flex relative">
-            {/* Badge */}
+            {/* Badge 1 */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={1}
+              badgeText="Años de experiencia"
+            />
+            {/* Badge 2 */}
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={4}
+              badgeText="Projectos terminados"
+            />
+            {/* Badge 3 */}
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={3}
+              badgeText="Clientes felices"
+            />
             <div
-              className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] 
-            h-[500px] bg-no-repeat absolute -top-1 -right-2"
-            >
-              <MyImg
-                containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative 
-              bg-bottom"
-                imgSrc="/hero/me.png"
-              />
-            </div>
-            {/* Icono */}
-            <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
-              <RiArrowDownSLine className="3xl text-primary" />
-            </div>
+              className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat
+            absolute -top-1 -right-2"
+            ></div>
+            <MyImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/me.png"
+            />
+          </div>
+          {/* Icono */}
+          <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+            <RiArrowDownSLine className="text-3xl text-primary" />
           </div>
         </div>
       </div>
